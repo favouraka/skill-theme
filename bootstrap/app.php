c64b28d7992e93caeae4b63ffc2c12c51c9e8c2d
Aka Favour <akafavour1@yahoo.com>
2024-10-08 14:55:56 +0100

    Trusts all proxies, cloudflare proxy support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
